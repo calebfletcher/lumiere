33a042abd3377192ad88f3c1172331c8a510a64d
Caleb Fletcher <caleb@fletcher.cf>
2021-11-15 21:23:45 +1100

    feat(material): add schlick approximation to dielectric

--- src/material/dielectric.rs
+++ src/material/dielectric.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use crate::{object::HitRecord, ray::Ray, Colour};
 
 use super::{Behaviour, Material, MaterialScatterResult};
@@ -11,6 +13,11 @@ impl Dielectric {
     pub fn new(ir: f64) -> Self {
         Self { ir }
     }
+
+    fn reflectance(&self, cosine: f64, ref_idx: f64) -> f64 {
+        let r0 = ((1. - ref_idx) / (1. + ref_idx)).powi(2);
+        r0 + (1. - r0) * (1. - cosine).powi(5)
+    }
 }
 
 impl Material for Dielectric {
@@ -18,7 +25,7 @@ impl Material for Dielectric {
         &self,
         r: &Ray,
         hitrec: &HitRecord,
-        _rng: &mut rand::rngs::ThreadRng,
+        rng: &mut rand::rngs::ThreadRng,
     ) -> MaterialScatterResult {
         let attenuation = Colour::new(1., 1., 1.);
         let refraction_ratio = if hitrec.front_face {
@@ -33,7 +40,8 @@ impl Material for Dielectric {
         let sin_theta = (1. - cos_theta.powi(2)).sqrt();
 
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
-        let direction = match cannot_refract {
+        let reflectance = self.reflectance(cos_theta, refraction_ratio);
+        let direction = match cannot_refract || reflectance > rng.gen() {
             true => unit_direction.reflect(&hitrec.normal.unit()),
             false => unit_direction.refract(&hitrec.normal.unit(), refraction_ratio),
         };
